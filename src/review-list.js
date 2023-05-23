f13009da18146d84cbf1aa3cbae14b8b96813732
anagalacticRuby <nick12.aded@gmail.com>
2023-05-23 14:44:00 -0400

    Lifted state, fixed reviewlist, added code 4 Stars

--- src/review-list.js
+++ src/review-list.js
@@ -5,21 +5,23 @@ import Stars from "./stars";
 export default class ReviewList extends React.Component {
   //Store the reviews in an array for this project
   //Reviews are created using the review-form component
-  constructor(props) {
-    super(props);
-    //if the movie component passes down the review array as state, then it becomes props for reviewlist
-  }
+  //if the movie component passes down the review array as state, then it becomes props for reviewlist
 
-   reviewItems = this.props.reviews.map(items =>
-    <li>
-        <Review reviewtext={items.reviews}/>
-    </li>);
+    reviewMap = this.props.reviews.map(function(element, index) {
+        return(<li key={index}>
+            <Review key={index} reviewtext={element}/>
+        </li>);
+    })
+//    reviewItems = this.props.reviews.map(items =>
+//     <li>
+//         <Review key={items} reviewtext={items}/>
+//     </li>);
   render() {
     return (
       <ul>
         {console.log(this.props.reviews)}
-        {this.reviewItems}
-        <Review reviewtext={this.props.reviews} />
+        {/* {this.reviewItems} */}
+        {this.reviewMap}
       </ul>
     );
   }
